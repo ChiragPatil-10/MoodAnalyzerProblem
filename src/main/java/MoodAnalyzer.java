019de35433222ddc9c7e1c4688592dd930419db9
Chirag Patil <patilchirag192k@gmail.com>
2024-12-19 15:50:17 +0530

    Welcome to mood analyzer

---
+++ src/main/java/MoodAnalyzer.java
@@ -0,0 +1,33 @@
+public class MoodAnalyzer {
+    private String mood;
+    public MoodAnalyzer(String mood) {
+        this.mood=mood;
+    }
+
+    public String analyzeMood() {
+        if(mood==null || mood.isEmpty()) {
+            return "Neutral";
+        }
+        if(mood.toLowerCase().contains("happy")) {
+            return "Happy";
+        }else if (mood.toLowerCase().contains("sad")) {
+            return "Sad";
+        }else {
+            return "Neutral";
+        }
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Mood Analyzer Problem");
+
+        MoodAnalyzer moodAnalyze1 = new MoodAnalyzer("Happy");
+        System.out.println("Mood: " + moodAnalyze1.analyzeMood());
+
+        MoodAnalyzer moodAnalyzer2 = new MoodAnalyzer("Sad");
+        System.out.println("Mood: " + moodAnalyzer2.analyzeMood());
+
+        MoodAnalyzer moodAnalyzer3 = new MoodAnalyzer("");
+        System.out.println("Mood: " + moodAnalyzer3.analyzeMood());
+    }
+}
