c239b65b5d4604aa659f80b90e5f3093dbcd0870
Chirag Patil <patilchirag192k@gmail.com>
2024-12-20 14:57:23 +0530

    Null mood case

--- src/main/java/MoodAnalyzer.java
+++ src/main/java/MoodAnalyzer.java
@@ -1,37 +1,66 @@
+class MoodAnalyzerException extends Exception {
+    public MoodAnalyzerException(String message) {
+        super(message);
+    }
+}
 public class MoodAnalyzer {
     private String mood;
 
     public MoodAnalyzer() {
-        mood = "HAPPY";
+        mood = "null";
     }
 
     public MoodAnalyzer(String mood) {
         this.mood=mood;
     }
 
-    public String analyzeMood() {
-        if(mood==null || mood.isEmpty())
-            return "SAD";
+    public String analyzeMood() throws MoodAnalyzerException {
+        if (mood == null)
+            return "Happy";
 
-        if(mood.toLowerCase().contains("happy")) {
+        if(mood.isEmpty())
+            throw new MoodAnalyzerException("Mood cannot be empty");
+
+        if (mood.toLowerCase().contains("happy")) {
             return "SAD";
-        }else if (mood.toLowerCase().contains("sad")) {
+        } else if (mood.toLowerCase().contains("sad")) {
             return "SAD";
-        }else  {
+        } else if (mood.toLowerCase().contains("any")) {
             return "HAPPY";
+    } else {
+            throw new MoodAnalyzerException("Invalid Mood");
         }
     }
 
     public static void main(String[] args) {
         System.out.println("Welcome to Mood Analyzer Problem");
 
-        MoodAnalyzer moodAnalyze1 = new MoodAnalyzer("Happy Mood");
-        System.out.println("Mood: " + moodAnalyze1.analyzeMood());
+        MoodAnalyzer moodAnalyzer1 = new MoodAnalyzer("Happy Mood");
+        try {
+            System.out.println("Mood: " + moodAnalyzer1.analyzeMood());
+        } catch (MoodAnalyzerException e) {
+            System.out.println("Exception : " + e.getMessage());
+        }
 
         MoodAnalyzer moodAnalyzer2 = new MoodAnalyzer("Sad Mood");
-        System.out.println("Mood: " + moodAnalyzer2.analyzeMood());
+        try {
+            System.out.println("Mood: " + moodAnalyzer2.analyzeMood());
+        } catch (MoodAnalyzerException e) {
+            System.out.println("Exception: " + e.getMessage());
+        }
 
         MoodAnalyzer moodAnalyzer3 = new MoodAnalyzer("");
-        System.out.println("Mood: " + moodAnalyzer3.analyzeMood());
+        try {
+            System.out.println("Mood: " + moodAnalyzer3.analyzeMood());
+        } catch (MoodAnalyzerException e) {
+            System.out.println("Exception: " + e.getMessage());
+        }
+
+        MoodAnalyzer moodAnalyzer4 = new MoodAnalyzer();
+        try {
+            System.out.println("Mood: " + moodAnalyzer4.analyzeMood());
+        } catch (MoodAnalyzerException e) {
+            System.out.println("Exception: " + e.getMessage());
+        }
     }
 }
--- src/test/java/MoodAnalyzerTest.java
+++ src/test/java/MoodAnalyzerTest.java
@@ -3,26 +3,33 @@ import static junit.framework.Assert.assertEquals;
 
 public class MoodAnalyzerTest {
     @Test
-    public void HappyOrSad() {
+    public void HappyOrSad() throws MoodAnalyzerException {
         MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am in Sad Mood");
         String result = moodAnalyzer.analyzeMood();
         assertEquals("SAD",result);
-
     }
 
     @Test
-    public void Mood() {
+    public void Mood() throws MoodAnalyzerException {
         MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am in any Mood");
         String result = moodAnalyzer.analyzeMood();
         assertEquals("HAPPY", result);
-
     }
 
     @Test
-    public void HappyMood() {
+    public void HappyMood() throws MoodAnalyzerException {
         MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am in Happy Mood");
         String result = moodAnalyzer.analyzeMood();
         assertEquals("SAD", result);
     }
 
+    @Test
+    public void NullScenario() {
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer(null);
+        try {
+            assertEquals("Happy",moodAnalyzer.analyzeMood());
+        } catch (MoodAnalyzerException e) {
+            System.out.println("Mood: " + e.getMessage());
+        }
+    }
 }
